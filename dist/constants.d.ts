6bc0f8aa4ff1b290d8675b874f1f7188885ca2b8
MoneshBlockchain <monesh@thoughtwin.com>
2022-08-29 13:36:44 +0530

    first commit

---
+++ dist/constants.d.ts
@@ -0,0 +1,8 @@
+import { AddressMap } from '@sushiswap/core-sdk';
+export declare const LAMBDA_URL = "https://limit-order-ffo5rqmjnq-uc.a.run.app";
+export declare const SOCKET_URL = "wss://limit-order-ffo5rqmjnq-uc.a.run.app";
+export declare const STOP_LIMIT_ORDER_ADDRESS: AddressMap;
+export declare const DEFAULT_RECEIVER_ADDRESS: AddressMap;
+export declare const ADVANCED_RECEIVER_ADDRESS: AddressMap;
+export declare const LIMIT_ORDER_HELPER_ADDRESS: AddressMap;
+export declare const ROUND_UP_RECEIVER_ADDRESS: AddressMap;
